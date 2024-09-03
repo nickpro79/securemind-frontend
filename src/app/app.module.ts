bf797727a6cf4796637a614d44b695c99717e973
Nikhil Praveen <nikhilpraveen7777@gmail.com>
2024-09-03 13:00:43 +0530

    Added LoginPage and RegisterPage Component
    Added Navbar to base page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginpageComponent } from './Components/loginpage/loginpage.component';
+import { RegisterpageComponent } from './Components/registerpage/registerpage.component';
+import { PagenotfoundComponent } from './Components/pagenotfound/pagenotfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginpageComponent,
+    RegisterpageComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
