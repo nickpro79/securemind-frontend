f72cb77ff522a148f254670153e424646611ae10
Nikhil Praveen <nikhilpraveen7777@gmail.com>
2024-09-04 10:45:48 +0530

    added the functionality for login button

--- src/app/Components/loginpage/loginpage.component.ts
+++ src/app/Components/loginpage/loginpage.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Login } from '../../Models/login';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-loginpage',
   templateUrl: './loginpage.component.html',
   styleUrl: './loginpage.component.css'
 })
-export class LoginpageComponent {
+export class LoginpageComponent implements OnInit {
+  data:Login={
+username:"",
+password:""
+  }
+  constructor(private _userservice:UserService){
 
+  }
+  ngOnInit(): void {
+
+  }
+  Login(username:HTMLInputElement,password:HTMLInputElement){
+    this.data.username = username.value
+    this.data.password = password.value
+    this._userservice.Login(this.data)
+    .subscribe({
+     next:(response)=>{console.log(response)},
+     error:(err)=>{console.log(err)}
+    })
+
+  }
 }
