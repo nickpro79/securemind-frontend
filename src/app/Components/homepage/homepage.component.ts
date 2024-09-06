92a71f1ed8ff1f8edadc804eba5b6799af1d9987
Prasanth.M2 <prasanth.m2@ust.com>
2024-09-06 17:15:54 +0530

    added necessary imports

--- src/app/Components/homepage/homepage.component.ts
+++ src/app/Components/homepage/homepage.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators,ValidatorFn, AbstractControl } from '@angular/forms';
 import L from 'leaflet';
 
