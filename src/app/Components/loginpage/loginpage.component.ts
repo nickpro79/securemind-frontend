9374f30290caf0f8d227d4fb1f5d241acd59072a
Nikhil Praveen <nikhilpraveen7777@gmail.com>
2024-09-06 12:59:58 +0530

    Added validations for Login Form

--- src/app/Components/loginpage/loginpage.component.ts
+++ src/app/Components/loginpage/loginpage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Login } from '../../Models/login';
 import { UserService } from '../../services/user.service';
 
@@ -8,16 +9,31 @@ import { UserService } from '../../services/user.service';
   styleUrl: './loginpage.component.css'
 })
 export class LoginpageComponent implements OnInit {
-  data:Login={
+data:Login={
 username:"",
 password:""
-  }
-  constructor(private _userservice:UserService){
+}
+loginForm:FormGroup=null!
 
-  }
-  ngOnInit(): void {
+constructor(private _userservice:UserService, private Fb:FormBuilder){}
 
+ngOnInit(): void {
+  this.loginForm = this.Fb.group({
+    username:['',Validators.required],
+    password:['',Validators.required]
+  })
   }
+
+  validateControl(input:string){
+    return this.loginForm.get(input)?.invalid &&
+    (this.loginForm.get(input)?.touched || this.loginForm.get(input)?.dirty)
+    }
+
+  validateControlError(input:string,errorType:string){
+      return this.loginForm.get(input)?.hasError(errorType) &&
+      (this.loginForm.get(input)?.touched || this.loginForm.get(input)?.dirty)
+    }
+
   Login(username:HTMLInputElement,password:HTMLInputElement){
     this.data.username = username.value
     this.data.password = password.value
@@ -26,6 +42,9 @@ password:""
      next:(response)=>{console.log(response)},
      error:(err)=>{console.log(err)}
     })
+  }
 
+  get validForm(){
+    return this.loginForm.valid
   }
 }
